990359ae164c05a79015ead5ad0031249b2f44ba
Daniel Voros <dvoros@hortonworks.com>
2018-05-16 16:20:47 +0200

    always run bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,10 +18,4 @@ $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
-fi
-
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
-fi
+/bin/bash
